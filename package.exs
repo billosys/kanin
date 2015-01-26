14476ff8ad5b65202b5890b0b23ac328341fc08d
Odakotah LaBlanc <odacoder@billo.systems>
2015-01-25 23:12:10 -0600

    Added lfe.config and removed obselete package.exs.

--- package.exs
+++
@@ -1,7 +0,0 @@
-Expm.Package.new(
-  name: "kanin",
-  description: "kanin DESCRIPTION",
-  version: "0.0.1",
-  keywords: ["LFE", "Lisp", "Library", "API"],
-  maintainers: [[name: "YOUR NAME", email: "YOUR@EMAIL.com"]],
-  repositories: [[github: "YOUR_GITHUB_NAME/kanin"]])
